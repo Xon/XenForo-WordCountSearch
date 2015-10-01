5575d7324d056f5be93f2c0e424b00258d2bf89b
Xon <tazipper1@hotmail.com>
2015-10-01 23:05:45 +0800

    Ensure word count data is indexed if it does not exist

--- upload/library/SV/SearchImprovements/XenForo/Search/DataHandler/Post.php
+++ upload/library/SV/SearchImprovements/XenForo/Search/DataHandler/Post.php
@@ -4,20 +4,28 @@ class SV_SearchImprovements_XenForo_Search_DataHandler_Post extends XFCP_SV_Sear
 {
     protected function _insertIntoIndex(XenForo_Search_Indexer $indexer, array $data, array $parentData = null)
     {
-        if (isset($data[SV_SearchImprovements_Globals::WordCountField]))
+        if (!isset($data[SV_SearchImprovements_Globals::WordCountField]))
         {
-            $metadata = array();
-            $metadata[SV_SearchImprovements_Globals::WordCountField] = $data[SV_SearchImprovements_Globals::WordCountField];
-
-            if ($indexer instanceof SV_SearchImprovements_Search_IndexerProxy)
-            {
-                $indexer->setProxyMetaData($metadata);
-            }
-            else
-            {
-                $indexer = new SV_SearchImprovements_Search_IndexerProxy($indexer, $metadata);
-            }
+            $wordcount = $this->_getPostModel()->getTextWordCount($data['message']);
+            $db = XenForo_Application::getDb();
+            $db->query("
+                insert ignore into xf_post_words (post_id, word_count) values (?,?)
+            ", array($data['post_id'], $wordcount));
+            $data[SV_SearchImprovements_Globals::WordCountField] = $wordcount;
         }
+
+        $metadata = array();
+        $metadata[SV_SearchImprovements_Globals::WordCountField] = $data[SV_SearchImprovements_Globals::WordCountField];
+
+        if ($indexer instanceof SV_SearchImprovements_Search_IndexerProxy)
+        {
+            $indexer->setProxyMetaData($metadata);
+        }
+        else
+        {
+            $indexer = new SV_SearchImprovements_Search_IndexerProxy($indexer, $metadata);
+        }
+        
 
         parent::_insertIntoIndex($indexer, $data, $parentData);
     }
