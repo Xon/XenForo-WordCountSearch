ae4851b34156ea71f2d494c1c8e8c72f5cc2f27a
Xon <tazipper1@hotmail.com>
2015-10-21 22:02:03 +0800

    Only show word count search when possible

--- upload/library/SV/WordCountSearch/XenForo/ControllerPublic/Search.php
+++ upload/library/SV/WordCountSearch/XenForo/ControllerPublic/Search.php
@@ -6,20 +6,24 @@ class SV_WordCountSearch_XenForo_ControllerPublic_Search extends XFCP_SV_WordCou
     {
         $response = parent::actionIndex();
 
-        if ($response instanceof XenForo_ControllerResponse_View && !empty($response->params['search']))
+        if ($response instanceof XenForo_ControllerResponse_View)
         {
-            $params = $this->_input->filter(array(
-                'c' => XenForo_Input::ARRAY_SIMPLE
-            ));
+            if (!empty($response->params['search']))
+            {
+                $params = $this->_input->filter(array(
+                    'c' => XenForo_Input::ARRAY_SIMPLE
+                ));
 
-            if (isset($params['c']['word_count'][0]))
-            {
-                $response->params['search']['word_count']['lower'] = $params['c']['word_count'][0];
+                if (isset($params['c']['word_count'][0]))
+                {
+                    $response->params['search']['word_count']['lower'] = $params['c']['word_count'][0];
+                }
+                if (isset($params['c']['word_count'][1]))
+                {
+                    $response->params['search']['word_count']['upper'] = $params['c']['word_count'][1];
+                }
             }
-            if (isset($params['c']['word_count'][1]))
-            {
-                $response->params['search']['word_count']['upper'] = $params['c']['word_count'][1];
-            }
+            $response->params['search']['range_query'] = class_exists('XFCP_SV_SearchImprovements_XenES_Search_SourceHandler_ElasticSearch', false);    
         }
 
         return $response;
