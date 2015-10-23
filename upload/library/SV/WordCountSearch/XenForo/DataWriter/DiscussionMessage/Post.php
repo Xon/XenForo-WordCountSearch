58e4a92835061216155926cf1dcf43a20142181f
Xon <tazipper1@hotmail.com>
2015-10-23 23:36:40 +0800

    Handle updates/deletes of linked tables correctly

--- upload/library/SV/WordCountSearch/XenForo/DataWriter/DiscussionMessage/Post.php
+++ upload/library/SV/WordCountSearch/XenForo/DataWriter/DiscussionMessage/Post.php
@@ -13,6 +13,27 @@ class SV_WordCountSearch_XenForo_DataWriter_DiscussionMessage_Post extends XFCP_
         return $fields;
     }
 
+    protected function _getExistingData($data)
+    {
+        $postData = parent::_getExistingData($data);
+        if (isset($postData['xf_post']['word_count']))
+        {
+            $postData['xf_post_words'] = array('post_id' => $postData['xf_post']['post_id'], 'word_count' => $postData['xf_post']['word_count']);
+            unset($postData['xf_post']['word_count']);
+        }
+        return $postData;
+    }
+
+    protected function _messagePostDelete()
+    {
+        parent::_messagePostDelete();
+        $db = $this->_db;
+        $post_id = $this->get('post_id');
+        $postIdQuoted = $db->quote($post_id);
+
+        $db->delete('xf_post_words', "post_id = $postIdQuoted");
+    }
+
     protected function _messagePreSave()
     {
         parent::_messagePreSave();
