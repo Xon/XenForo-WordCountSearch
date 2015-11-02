9968d62c2aa093172a195ed16b1ddb6d308085dc
Xon <tazipper1@hotmail.com>
2015-11-02 19:24:56 +0800

    Support copying posts.

--- upload/library/SV/WordCountSearch/XenForo/Model/Post.php
+++ upload/library/SV/WordCountSearch/XenForo/Model/Post.php
@@ -28,6 +28,26 @@ class SV_WordCountSearch_XenForo_Model_Post extends XFCP_SV_WordCountSearch_XenF
         return $post;
     }
 
+    protected function _copyPost(array $post, array $targetThread, array $forum)
+    {
+        $wordcount = null;
+        if (isset($post['word_count']))
+        {
+            $wordcount = $post['word_count'];
+            unset($post['word_count']);
+        }
+
+        $newPost = parent::_copyPost($post, $targetThread, $forum);
+
+        if ($wordcount !== null)
+        {
+            $db = XenForo_Application::getDb();
+            $db->query("
+                insert ignore into xf_post_words (post_id, word_count) values (?,?)
+            ", array($newPost['post_id'], $wordcount));
+        }
+    }
+
     protected function _getSearchModel()
     {
         return $this->getModelFromCache('XenForo_Model_Search');
