da7d97108b73d43d5f9d0706b5c67d9358f6b008
Xon <tazipper1@hotmail.com>
2016-06-11 06:52:22 +0800

    Fix 'optimize index' support.

--- upload/library/SV/WordCountSearch/XenES/Model/Elasticsearch.php
+++ upload/library/SV/WordCountSearch/XenES/Model/Elasticsearch.php
@@ -2,6 +2,29 @@
 
 class SV_WordCountSearch_XenES_Model_ElasticSearch extends XFCP_SV_WordCountSearch_XenES_Model_ElasticSearch
 {
+	public function getOptimizableMappings(array $mappingTypes = null, $mappings = null)
+	{
+		if ($mappingTypes === null)
+		{
+			$mappingTypes = $this->getAllSearchContentTypes();
+		}
+		if ($mappings === null)
+		{
+			$mappings = $this->getMappings();
+		}
+        return $this->_getOptimizableMappings($mappingTypes, $mappings, []);
+	}
+
+	protected function _getOptimizableMappings(array $mappingTypes, $mappings, $extraMappings)
+	{
+        $extraMappings = array_merge($extraMappings, SV_WordCountSearch_Installer::$extraMappings);
+        if (is_callable('parent::_getOptimizableMappings'))
+        {
+            return parent::_getOptimizableMappings($mappingTypes, $mappings, $extraMappings);
+        }
+        return SV_Utils_ElasticSearch::getOptimizableMappings($mappingTypes, $mappings, $extraMappings);
+    }
+
     public function optimizeMapping($type, $deleteFirst = true, array $extra = array())
     {
         if (isset(SV_WordCountSearch_Installer::$extraMappings[$type]))
