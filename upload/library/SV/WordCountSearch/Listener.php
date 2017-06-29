07cfd195b08da3e9188fa02129b0215f2dd6f3b6
Xon <tazipper1@hotmail.com>
2017-06-29 15:18:01 +0800

    cleanup

--- upload/library/SV/WordCountSearch/Listener.php
+++ upload/library/SV/WordCountSearch/Listener.php
@@ -2,10 +2,8 @@
 
 class SV_WordCountSearch_Listener
 {
-    const AddonNameSpace = 'SV_WordCountSearch_';
-
     public static function load_class($class, array &$extend)
     {
-        $extend[] = self::AddonNameSpace.$class;
+        $extend[] = 'SV_WordCountSearch_' . $class;
     }
 }
