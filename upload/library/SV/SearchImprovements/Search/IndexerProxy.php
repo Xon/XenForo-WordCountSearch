794ee133e388bc61b6a4a0bf0ecb1870687ab55b
Xon <tazipper1@hotmail.com>
2017-06-29 23:19:23 +0800

    Add dependency so the add-on doesn't break sites.

---
+++ upload/library/SV/SearchImprovements/Search/IndexerProxy.php
@@ -0,0 +1,25 @@
+<?php
+
+class SV_SearchImprovements_Search_IndexerProxy extends XenForo_Search_Indexer
+{
+    protected $_proxiedIndexer = null;
+    protected $_metadata = array();
+
+    public function __construct(XenForo_Search_Indexer $otherIndexer, array $metadata)
+    {
+        $this->_sourceHandler = $otherIndexer->_sourceHandler;
+        $this->_proxiedIndexer = $otherIndexer;
+        $this->_metadata = $metadata;
+    }
+
+    public function setProxyMetaData(array $metadata)
+    {
+        $this->_metadata = $metadata;
+    }
+
+    public function insertIntoIndex($contentType, $contentId, $title, $message, $itemDate, $userId, $discussionId = 0, array $metadata = array())
+    {
+        $metadata = XenForo_Application::mapMerge($metadata, $this->_metadata);
+        $this->_proxiedIndexer->insertIntoIndex($contentType, $contentId, $title, $message, $itemDate, $userId, $discussionId, $metadata);
+    }
+}
