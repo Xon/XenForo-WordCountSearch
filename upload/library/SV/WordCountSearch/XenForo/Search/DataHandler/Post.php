dddf58c97aa7590c5a516ff7497a61c814350702
Xon <tazipper1@hotmail.com>
2017-07-04 21:30:38 +0800

    Remove redundant code as extending the search source handler implements sorting

--- upload/library/SV/WordCountSearch/XenForo/Search/DataHandler/Post.php
+++ upload/library/SV/WordCountSearch/XenForo/Search/DataHandler/Post.php
@@ -62,18 +62,6 @@ class SV_WordCountSearch_XenForo_Search_DataHandler_Post extends XFCP_SV_WordCou
         return parent::getSearchFormControllerResponse($controller, $input, $viewParams);
     }
 
-    public function getOrderClause($order)
-    {
-        if ($order == 'word_count')
-        {
-            return array(
-                array('search_index', 'word_count', 'desc'),
-                array('search_index', 'item_date', 'desc')
-            );
-        }
-        return parent::getOrderClause($order);
-    }
-
     /**
      * @var SV_WordCountSearch_XenForo_Model_Search
      */
